6a48b4546b009263cee8273d8a7b5c688d64ed3c
yogarevy <yogarevysetiadi@gmail.com>
2021-03-09 19:08:48 +0700

    Create access middleware

---
+++ app/Http/Middleware/AccessMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Libraries\ResponseStd;
+use App\Models\User;
+use Closure;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class AccessMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $user = User::where('id', auth('api')->user()->id)->first();
+        $is_seller = $user['is_seller'];
+        if ($is_seller) {
+            return $next($request);
+        }
+
+        return ResponseStd::fail($errors = 'Unauthorized.', JsonResponse::HTTP_FORBIDDEN, $messages = 'Error access.');
+    }
+}
